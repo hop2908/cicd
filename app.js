bf6aa113392311bfa11319ae25b0c86afdd53520
hop2908 <hophuynh.29082004@gmail.com>
2025-10-14 20:21:24 +0700

    up

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello CI/CD with Docker & GitHub Actions!\n');
+  res.end('Hello CI/CD with Docker & GitHub Actionssssss!\n');
 });
 
 server.listen(port, () => {
